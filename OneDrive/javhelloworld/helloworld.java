b5c4cac7b7ac28ae97452c196299cea09ef5b784
DOTRIAN <dotrian10@gmail.com>
2024-10-24 14:53:09 +0700

    Complete assignment 2.2.1

---
+++ OneDrive/javhelloworld/helloworld.java
@@ -0,0 +1,10 @@
+//Example 1:helloworld.java
+//Text-printing program
+public class helloworld {
+
+  public static void main(String args[]){
+      System.out.println("xin chao cac ban");
+      System.out.println("hello world");
+  }
+}
+
